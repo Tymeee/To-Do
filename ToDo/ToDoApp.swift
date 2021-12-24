7cf761ce406b7e091360f280df9878a4b2099fd3
Tymeee <88166167+Tymeee@users.noreply.github.com>
2021-12-25 00:51:45 +0700

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by E2318556 on 25/12/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
